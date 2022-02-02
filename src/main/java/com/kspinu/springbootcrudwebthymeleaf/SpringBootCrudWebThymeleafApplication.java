1ef38e6c0068dd2ef12e9719e61c5bedcfc16804
Camelia Spinu <kspinudev@gmail.com>
2022-02-02 21:03:02 +0200

    init

---
+++ src/main/java/com/kspinu/springbootcrudwebthymeleaf/SpringBootCrudWebThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.kspinu.springbootcrudwebthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudWebThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCrudWebThymeleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kspinu/springbootcrudwebthymeleaf/SpringBootCrudWebThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kspinu.springbootcrudwebthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudWebThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
